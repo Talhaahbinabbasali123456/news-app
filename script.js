dc1652b23f0eec679c17a6506a84518e20f9e934
Talhaah Abbas <38943204+Talhaahbinabbasali123456@users.noreply.github.com>
2018-08-26 08:22:26 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,62 @@
-function call() {
-
-
-let backgroundT = ["images/4.jpg", "images/news.png", "images/Fre.jpg"];
-let randomT = parseInt(Math.random()*3);
-	document.querySelector(".header-layout").style["background-image"] = `url(${backgroundT[randomT]})`;
-
-let backgrounds = ["images/2.jpg", "images/3.jpg", "images/4.jpg"];
-let randoms = parseInt(Math.random()*3);
-	document.querySelector(".main-article-layout").style["background-image"] = `url(${backgrounds[randoms]})`;
-
-}
-setInterval(call,3000);
 
 
 
+let arrayOfNavList =
+	 ["Top headline",	
+	"Showbiz",
+	"Weather", 
+	"Games", 
+	"Media",
+	"Politics", 
+	"Social", 	
+	"Animal Planet", 
+	"Hollywood", 
+	"Sports"];
+
+let dummiText = "khkask gfjkasghjk gsfasfuisdtfa igfasfgtasuity asigfagfuiasy guasghdlsgdgh gdhsgsdhigh";
+let dummiTime = "‎‎12:00 PM 26 August ‎2018";
 
 
+function create() {
 
 
-
-let catogoriesName = ["Top Headlines", "Politics", "Sports", "Showbiz", "Weather", "Animal planet", "Technology", "Internet", "Cooking", "Fitness"];
-
-
-function inicialize() {
 $.ajax({
-	url : "https://newsapi.org/v2/top-headlines?sources=football-italia&apiKey=8c8793ffe9c046b6a6c2b3f7313f5e3a",
+	url : "https://newsapi.org/v2/top-headlines?sources=bild&apiKey=ff3f343e7f1448b29ed977f68f7f2c0f",
 	success: function(data){
 		console.log(data)
 
-	for(let i=0; i<8; i++){
-	document.querySelector(".article-content-2").innerHTML += `<div class='news'>
-	<img  class="img" src="${data.articles[i].urlToImage}">
-	<h1 class='desc'>${data.articles[i].description}<span class='date'>${data.articles[i].publishedAt}</span></h1>
-	
-	<br>
-	
-	</div>`
+	for (var i = 0; i < 10; i++) {
+		document.querySelector(".main-content").innerHTML += 
+		`<div class='news-container'>
+			<img class='image-div' src='${data.articles[i].urlToImage}'>
+
+		 	<div class='headline-div'>
+		 		<p class='news-text'>${data.articles[i].description}</p>
+		 		<span class='news-time'>${data.articles[i].publishedAt}</span>
+		 	</div>
+		 </div>`
 	}
+	
 }
  })
- for(let i=0; i<8; i++){
-	// console.log("lanti")
-document.querySelector(".article-content").innerHTML += `<div class='catagories'>${catogoriesName[i]}</div>`
+
+
+
+	for (var i = 0; i < 10; i++) {
+		document.querySelector(".main-nav").innerHTML += `<span class='nav-item'>${arrayOfNavList[i]}</span>`
+	}
 }
-}
+
+
+
+function callback() {
+	let random = parseInt(Math.random()*100+80);
+	document.querySelector(".main-nav").style.transition = 3+"s";
+	document.querySelector(".main-haader").style.transition = 3+"s";
+      document.querySelector(".main-footer").style.transition = 3+"s";
+	document.querySelector(".main-nav").style["background-size"] = random*2 + "%";
+	document.querySelector(".main-haader").style["background-size"] = random*2 + "%";
+      document.querySelector(".main-footer").style["background-size"] = random*2 + "%";
+  
+} 
+setInterval(callback, 5000);
